1d64aa47bad67b2720c63d8b2ec8e0bdc9f2293f
Kent Fredric <kentfredric@gmail.com>
2017-03-02 11:58:17 +1300

    Build results of 4790bd7 (on master)

--- lib/Dist/Zilla/Plugin/Prereqs/Upgrade.pm
+++ lib/Dist/Zilla/Plugin/Prereqs/Upgrade.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Prereqs::Upgrade;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.001001';
 
 # ABSTRACT: Upgrade existing prerequisites in place
 
@@ -181,7 +181,7 @@ Dist::Zilla::Plugin::Prereqs::Upgrade - Upgrade existing prerequisites in place
 
 =head1 VERSION
 
-version 0.001000
+version 0.001001
 
 =head1 DESCRIPTION
 
@@ -317,7 +317,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2017 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
